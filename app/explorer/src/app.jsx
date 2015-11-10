600d8d375e4954ec9df3f0e216e73d763c07e26c
Alexis Jacomy <alexis.jacomy@gmail.com>
2015-11-10 19:23:47 +0100

    Started plugging explore app logic
    Details:
     - src/controller.js will contain the different "actions", that work through Baobab's event dispatcher
     - The filters list has been refactored and plugged to the logic
     - Some required metrics are now computed when the data is loaded: Right now, it only concerns values lists for some fields
     - The filters are now fully represented in the tree

--- app/explorer/src/app.jsx
+++ app/explorer/src/app.jsx
@@ -8,6 +8,7 @@ var _ = require('lodash'),
     BaobabRootMixin = require('baobab-react/mixins').root,
 
     tree = require('./tree.js'),
+    controller = require('./controller.js'),
     DataStore = require('./utils/datastore'),
     HashBinder = require('./utils/hashbinder'),
     container = document.getElementById('app-container'),
@@ -23,17 +24,25 @@ var _ = require('lodash'),
 djax({
   url: 'assets/data/config.json',
   success: function(config) {
+    // Cache config:
+    tree.set(
+      ['cached', 'config'],
+      config
+    );
+
     // Load CSV data file:
     Papa.parse('assets/data/data.csv', {
       header: true,
       download: true,
-      complete: function(results) {
-        // Feed the store:
-        var store = new DataStore({
+      complete: function(data) {
+        // Reference the store as a tree attribute, to make it easily accessible
+        // from outside of here:
+        tree.datastore = new DataStore({
           fields: config.fields,
           index: config.index
         });
 
+        // Feed the store:
         var splitables = [];
         _.forEach(config.fields, function(field, key) {
           if (field.separator)
@@ -43,16 +52,13 @@ djax({
             });
         });
 
-        results.data.forEach(function(row) {
+        data.data.forEach(function(row) {
           splitables.forEach(function(field) {
-            row[field.id] = (row[field.id] || '').split(field.separator);
+            row[field.id] =
+              (row[field.id] || '').split(field.separator) || undefined;
           })
-          store.append(row);
+          tree.datastore.append(row);
         });
-
-        // Reference the store as a tree attribute, to make it easily accessible
-        // from outside of here:
-        tree.datastore = store;
 
         // Hash bindings:
         new HashBinder(
@@ -60,10 +66,34 @@ djax({
           ['deployedList', 'deployedVerbatim']
         );
 
-        // Initial rendering:
-        ReactDOM.render(
-          <App tree={ tree } />,
-          container
+        // Compute once and for all some relevant metrics:
+        var aggregations = {};
+        _.forEach(config.fields, function(obj, field) {
+          if (obj.cacheValues)
+            aggregations[field] = field;
+        });
+
+        tree.datastore.query(
+          { aggregations: aggregations },
+          function(result) {
+            for (var k in result.aggregations)
+              result.aggregations[k] =
+                _.map(result.aggregations[k], function(value, key) {
+                  return key;
+                });
+
+            // Cache values lists:
+            tree.set(
+              ['cached', 'valuesLists'],
+              result.aggregations
+            );
+
+            // Initial rendering:
+            ReactDOM.render(
+              <App tree={ tree } />,
+              container
+            );
+          }
         );
       }
     });
--- app/explorer/src/tree.js
+++ app/explorer/src/tree.js
@@ -3,9 +3,30 @@
 var Baobab = require('baobab');
 
 module.exports = new Baobab({
+  cached: {
+    valuesLists: {}
+  },
+
   appState: {
     filters: [],
     deployedList: undefined,
     deployedVerbatim: undefined
+  },
+
+  views: {
+    filtersIndex: Baobab.monkey({
+      cursors: {
+        filters: ['appState', 'filters']
+      },
+      get: function(data) {
+        return data.filters.reduce(function(res, filter) {
+          res[filter.field] = filter.values.reduce(function(res, value) {
+            res[value] = true;
+            return res;
+          }, {});
+          return res;
+        }, {})
+      }
+    })
   }
 });
