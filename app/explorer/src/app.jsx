7286c6b089f801478b4ae0fa22031e4f542cc82d
Alexis Jacomy <alexis.jacomy@gmail.com>
2015-11-10 12:06:42 +0100

    Now loading data and config files before rendering
    Details:
     - Added papaparse, djax and lodash as dependencies
     - Added utils/frontstore.js, the draft for the filtering and aggregation engine
     - Added draft files for data and config
     - Rewrote initialisation process, that loads the two files before the first rendering

--- app/explorer/src/app.jsx
+++ app/explorer/src/app.jsx
@@ -1,10 +1,14 @@
 'use strict';
 
-var React = require('react'),
+var _ = require('lodash'),
+    djax = require('djax'),
+    React = require('react'),
+    Papa = require('papaparse'),
     ReactDOM = require('react-dom'),
     BaobabRootMixin = require('baobab-react/mixins').root,
 
     tree = require('./tree.js'),
+    DataStore = require('./utils/datastore'),
     container = document.getElementById('app-container'),
     Layout = require('./components/Layout.jsx'),
     App = React.createClass({
@@ -14,10 +18,50 @@ var React = require('react'),
       }
     });
 
-ReactDOM.render(
-  <App tree={ tree } />,
-  container
-);
+// Load JSON config file:
+djax({
+  url: 'assets/data/config.json',
+  success: function(config) {
+    // Load CSV data file:
+    Papa.parse('assets/data/data.csv', {
+      header: true,
+      download: true,
+      complete: function(results) {
+        // Feed the store:
+        var store = new DataStore({
+          fields: config.fields,
+          index: config.index
+        });
+
+        var splitables = [];
+        _.forEach(config.fields, function(field, key) {
+          if (field.separator)
+            splitables.push({
+              id: key,
+              separator: field.separator
+            });
+        });
+
+        results.data.forEach(function(row) {
+          splitables.forEach(function(field) {
+            row[field.id] = (row[field.id] || '').split(field.separator);
+          })
+          store.append(row);
+        });
+
+        // Reference the store as a tree attribute, to make it easily accessible
+        // from outside of here:
+        tree.datastore = store;
+
+        // Initial rendering:
+        ReactDOM.render(
+          <App tree={ tree } />,
+          container
+        );
+      }
+    });
+  }
+})
 
 module.exports = {
   tree: tree
