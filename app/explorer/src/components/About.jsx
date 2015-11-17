753d8fc0a71f6f0f0796f74ae2e63acafcea4c60
Jérémy Lesceau <jlesceau@ateliericeberg.fr>
2015-11-17 13:19:23 +0100

    New header in both pages

--- app/explorer/src/components/About.jsx
+++ app/explorer/src/components/About.jsx
@@ -30,18 +30,26 @@ module.exports = React.createClass({
                 onClick={ this.close }></div>
           <div className="about-logo"></div>
           <div className="about-text">
-            <p>The Climate Negotiation allows the user to navigate over 20 years of UN climate negotiations as captured by the Earth Negotiation Bulletin reporting system.</p>
-            <p>The DISCOVER interface offers a visual overview of the most visible issues and actors of the negotiations.</p>
-            <p>The EXPLORE interface allows the user to search, filter and read verbatim text directly from ENB.</p>
+            <p>The Climate Negotiation allows the user to navigate over 20 years of UN climate negotiations as captured by the <a href="http://www.iisd.ca/vol12/" target="_blank">Earth Negotiation Bulletin</a> reporting system <br /><br /></p>
+            <p>The DISCOVER interface offers a visual overview of the most visible issues and actors of the negotiations.<br />The EXPLORE interface allows the user to search, filter and read verbatim text directly from ENB.</p>
           </div>
           <div className="read-more">Read more</div>
           <div className="about-partners">
-            <img  src="assets/img/sciences-po-medialab.svg"
-                  alt="Medialab SciencesPo" />
-            <img  src="assets/img/epfl.svg"
-                  alt="EPFL" />
-            <img  src="assets/img/atelier-iceberg.svg"
-                  alt="Atelier Iceberg" />
+            <a  href="http://www.medialab.sciences-po.fr/"
+                target="_blank">
+              <img  src="assets/img/sciences-po-medialab.svg"
+                    alt="Medialab SciencesPo" />
+            </a>
+            <a  href="http://lsir.epfl.ch/"
+                target="_blank">
+              <img  src="assets/img/epfl.svg"
+                    alt="EPFL" />
+            </a>
+            <a  href="http://ateliericeberg.fr/"
+                target="_blank">
+              <img  src="assets/img/atelier-iceberg.svg"
+                    alt="Atelier Iceberg" />
+            </a>
           </div>
         </div>
       </div>
