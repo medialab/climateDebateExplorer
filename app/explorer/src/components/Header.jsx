01a0b65ba181848415ebeb48bb88865e7099bda6
Jérémy Lesceau <jlesceau@ateliericeberg.fr>
2015-11-17 18:26:50 +0100

    [explore] Sharing buttons action

--- app/explorer/src/components/Header.jsx
+++ app/explorer/src/components/Header.jsx
@@ -13,6 +13,44 @@ module.exports = React.createClass({
   toggleAbout: function(e) {
     this.cursors.deployed.set(!this.state.deployed);
   },
+  share: function(e) {
+    window.open(
+      this._makeUrl(
+        e.currentTarget.getAttribute('data-network'),
+        encodeURIComponent('Climate Negociations Browser'),
+        encodeURIComponent(window.location.href)
+      ),
+      null,
+      'status=1,' +
+      'width=700,' +
+      'height=400,' +
+      'top=' + ( window.innerHeight / 2 - 200 ) + ',' +
+      'left=' + ( window.innerWidth / 2 - 350 )
+    );
+  },
+
+  _makeUrl: function(network, title, url) {
+    var networks = {
+      facebook: {
+        api: 'http://www.facebook.com/sharer/sharer.php?',
+        title: 'title=',
+        url: '&u='
+      },
+      twitter: {
+        api: 'http://twitter.com/intent/tweet?',
+        title: 'status='
+      },
+      linkedin: {
+        api: 'http://www.linkedin.com/shareArticle?mini=true&',
+        title: 'title=',
+        url: '&url='
+      }
+    };
+
+    return networks[network].api +
+      ( networks[network].title ? networks[network].title + title : '' ) +
+      ( networks[network].url ? networks[network].url + url : '' );
+  },
 
   render: function() {
     return (
@@ -28,9 +66,15 @@ module.exports = React.createClass({
               onClick={ this.toggleAbout }>About</a>
         </div>
         <div className="social">
-          <a className="button"><img src="assets/img/facebook.svg" /></a>
-          <a className="button"><img src="assets/img/twitter.svg" /></a>
-          <a className="button"><img src="assets/img/linkedin.svg" /></a>
+          <a  className="button"
+              data-network="facebook"
+              onClick={ this.share }><img src="assets/img/facebook.svg" /></a>
+          <a  className="button"
+              data-network="twitter"
+              onClick={ this.share }><img src="assets/img/twitter.svg" /></a>
+          <a  className="button"
+              data-network="linkedin"
+              onClick={ this.share }><img src="assets/img/linkedin.svg" /></a>
         </div>
         <div className="menu">
           <a  className="discover"
