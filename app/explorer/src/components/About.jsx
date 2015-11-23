682991f42b61d93d57b23a17fd10a784a70eda69
Alexis Jacomy <alexis.jacomy@gmail.com>
2015-11-23 16:21:12 +0100

    [explorer] Cleaned iframe integration in About panel

--- app/explorer/src/components/About.jsx
+++ app/explorer/src/components/About.jsx
@@ -24,45 +24,53 @@ module.exports = React.createClass({
                 'about' :
                 'about hidden'
             }
-            onClick={ this.close }>
-        <div className="about-content">
-          <div  className="about-cross"
-                onClick={ this.close }></div>
-          <div className="about-logo"></div>
-          <div className="about-text">
-            <table>
-              <tr>
-                <td>
-                  <iframe src="https://player.vimeo.com/video/146385070" width="250" height="157" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
-                </td>
-                <td className="spacer"></td>
-                <td>
-            <p>The Climate Negotiations Browser allows the user to navigate over 20 years of UN climate negotiations as captured by the <a href="http://www.iisd.ca/vol12/" target="_blank">Earth Negotiation Bulletin</a> reporting system.</p>
-            <p>The DISCOVER interface offers a visual overview of the most visible issues and actors of the negotiations.<br />The EXPLORE interface allows the user to search, filter and read verbatim text directly from ENB.</p>
-                </td>
-              </tr>
-            </table>
-          </div>
-          <div className="read-more">Read more</div>
-          <div className="about-partners">
-            <a  href="http://www.medialab.sciences-po.fr/"
-                target="_blank">
-              <img  src="../assets/img/sciences-po-medialab.svg"
-                    alt="Medialab SciencesPo" />
-            </a>
-            <a  href="http://lsir.epfl.ch/"
-                target="_blank">
-              <img  src="../assets/img/epfl.svg"
-                    alt="EPFL" />
-            </a>
-            <a  href="http://ateliericeberg.fr/"
-                target="_blank">
-              <img  src="../assets/img/atelier-iceberg.svg"
-                    alt="Atelier Iceberg" />
-            </a>
-          </div>
-        </div>
-      </div>
+            onClick={ this.close }>{
+        this.state.deployed ?
+          <div className="about-content">
+            <div  className="about-cross"
+                  onClick={ this.close }></div>
+            <div className="about-logo"></div>
+            <div className="about-text">
+              <table>
+                <tbody>
+                  <tr>
+                    <td>
+                      <iframe width="250"
+                              height="157"
+                              frameBorder="0"
+                              allowFullScreen
+                              src="https://player.vimeo.com/video/146385070" />
+                    </td>
+                    <td className="spacer"></td>
+                    <td>
+                      <p>The Climate Negotiations Browser allows the user to navigate over 20 years of UN climate negotiations as captured by the <a href="http://www.iisd.ca/vol12/" target="_blank">Earth Negotiation Bulletin</a> reporting system.</p>
+                      <p>The DISCOVER interface offers a visual overview of the most visible issues and actors of the negotiations.<br />The EXPLORE interface allows the user to search, filter and read verbatim text directly from ENB.</p>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <div className="read-more">Read more</div>
+            <div className="about-partners">
+              <a  href="http://www.medialab.sciences-po.fr/"
+                  target="_blank">
+                <img  src="../assets/img/sciences-po-medialab.svg"
+                      alt="Medialab SciencesPo" />
+              </a>
+              <a  href="http://lsir.epfl.ch/"
+                  target="_blank">
+                <img  src="../assets/img/epfl.svg"
+                      alt="EPFL" />
+              </a>
+              <a  href="http://ateliericeberg.fr/"
+                  target="_blank">
+                <img  src="../assets/img/atelier-iceberg.svg"
+                      alt="Atelier Iceberg" />
+              </a>
+            </div>
+          </div> :
+          undefined
+      }</div>
     );
   }
 });
