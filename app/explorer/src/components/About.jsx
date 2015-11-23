ede982c644866c15e4699f448de7f82bc42f08f2
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2015-11-23 14:37:38 +0100

    include video in explorer's about

--- app/explorer/src/components/About.jsx
+++ app/explorer/src/components/About.jsx
@@ -30,8 +30,18 @@ module.exports = React.createClass({
                 onClick={ this.close }></div>
           <div className="about-logo"></div>
           <div className="about-text">
-            <p>The Climate Negotiations Browser allows the user to navigate over 20 years of UN climate negotiations as captured by the <a href="http://www.iisd.ca/vol12/" target="_blank">Earth Negotiation Bulletin</a> reporting system.<br /><br /></p>
+            <table>
+              <tr>
+                <td>
+                  <iframe src="https://player.vimeo.com/video/146385070" width="250" height="157" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+                </td>
+                <td class="spacer"></td>
+                <td>
+            <p>The Climate Negotiations Browser allows the user to navigate over 20 years of UN climate negotiations as captured by the <a href="http://www.iisd.ca/vol12/" target="_blank">Earth Negotiation Bulletin</a> reporting system.</p>
             <p>The DISCOVER interface offers a visual overview of the most visible issues and actors of the negotiations.<br />The EXPLORE interface allows the user to search, filter and read verbatim text directly from ENB.</p>
+                </td>
+              </tr>
+            </table>
           </div>
           <div className="read-more">Read more</div>
           <div className="about-partners">
