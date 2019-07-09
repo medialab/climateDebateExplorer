eb42b15dfbed8a54d832e318b405cd8c0f822506
Yomguithereal <guillaumeplique@gmail.com>
2019-07-09 11:36:01 +0200

    Updates

---
+++ assets/src/bulletin.js
@@ -0,0 +1,18 @@
+// Fix chrome anchor bug
+(function() {
+  var el,
+      hash = (location.hash || '').replace(/^#/, '');
+
+  if ((el = document.getElementById('section_' + hash)))
+    window.setTimeout(function() {
+      window.scrollTo(0, el.offsetTop);
+      el.setAttribute('class', 'target');
+    }, 0);
+}).call(window);
+
+// Glossary
+(function(){
+
+  // TODO
+  
+})();
